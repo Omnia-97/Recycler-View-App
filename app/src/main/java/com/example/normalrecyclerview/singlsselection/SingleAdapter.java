b4b3ff8a55aa49792546da5f6bba6d6aa359bd0d
DELL <omnianabil1920@gmail.com>
2024-08-03 16:29:04 +0300

    recycler view with single selected

--- app/src/main/java/com/example/normalrecyclerview/singlsselection/SingleAdapter.java
+++ app/src/main/java/com/example/normalrecyclerview/singlsselection/SingleAdapter.java
@@ -1,4 +1,94 @@
 package com.example.normalrecyclerview.singlsselection;
 
-public class SingleAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.normalrecyclerview.R;
+
+import java.util.ArrayList;
+
+public class SingleAdapter extends RecyclerView.Adapter<SingleAdapter.EmployeeHolder> {
+    private Context context;
+    private ArrayList<Employee> employeeArrayList;
+    private int checkedPosition = 0;
+
+    public SingleAdapter(Context context, ArrayList<Employee> employeeArrayList) {
+        this.context = context;
+        this.employeeArrayList = employeeArrayList;
+    }
+
+    public void setEmployeeArrayList(ArrayList<Employee> employeeArrayList) {
+        this.employeeArrayList = new ArrayList<>();
+        this.employeeArrayList = employeeArrayList;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public EmployeeHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_employee, parent, false);
+
+        return new EmployeeHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EmployeeHolder holder, int position) {
+        holder.bind(employeeArrayList.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return employeeArrayList.size();
+    }
+
+    public class EmployeeHolder extends RecyclerView.ViewHolder {
+        TextView name;
+        ImageView imageView;
+
+
+        public EmployeeHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.emp_name);
+            imageView = itemView.findViewById(R.id.imageview_emp);
+        }
+
+        void bind(final Employee employee) {
+            if (checkedPosition == -1) {
+                imageView.setVisibility(View.GONE);
+            } else {
+                if (checkedPosition == getAdapterPosition()) {
+                    imageView.setVisibility(View.VISIBLE);
+                } else {
+                    imageView.setVisibility(View.GONE);
+                }
+
+            }
+            name.setText(employee.getName());
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    imageView.setVisibility(View.VISIBLE);
+                    if (checkedPosition != getAdapterPosition()) {
+                        notifyItemChanged(checkedPosition);
+                        checkedPosition = getAdapterPosition();
+                    }
+                }
+            });
+        }
+    }
+
+    public Employee getSelected() {
+        if (checkedPosition != -1) {
+            return employeeArrayList.get(checkedPosition);
+        }
+        return null;
+    }
 }
--- app/src/main/java/com/example/normalrecyclerview/singlsselection/SingleSelectionRV.java
+++ app/src/main/java/com/example/normalrecyclerview/singlsselection/SingleSelectionRV.java
@@ -1,16 +1,67 @@
 package com.example.normalrecyclerview.singlsselection;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.normalrecyclerview.R;
 
+import java.util.ArrayList;
+
 public class SingleSelectionRV extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private Button btn;
+
+    // Variables
+    private ArrayList<Employee> employees = new ArrayList<>();
+    private SingleAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_selection_rv);
+        recyclerView = findViewById(R.id.singleRV);
+        btn = findViewById(R.id.button_get_selected);
+
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        adapter = new SingleAdapter(this, employees);
+        recyclerView.setAdapter(adapter);
+        createList();
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (adapter.getSelected() != null) {
+                    ShowToast(adapter.getSelected().getName());
+
+                } else {
+                    ShowToast("No Employee Selected");
+
+
+                }
+            }
+        });
+    }
+
+    private void createList() {
+        employees = new ArrayList<>();
+
+        for (int i = 0; i < 20; i++) {
+            Employee employee = new Employee();
+            employee.setName("Employee " + (i+1));
+            employees.add(employee);
+        }
+        adapter.setEmployeeArrayList(employees);
+    }
+
+    private void ShowToast (String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
