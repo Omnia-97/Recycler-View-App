e9f98c10097ac6159edff4d236a89ef89aaddd62
DELL <omnianabil1920@gmail.com>
2024-08-03 14:48:13 +0300

    recycler view with card view

--- app/src/main/java/com/example/normalrecyclerview/cardview/CardAdapter.java
+++ app/src/main/java/com/example/normalrecyclerview/cardview/CardAdapter.java
@@ -1,5 +1,7 @@
 package com.example.normalrecyclerview.cardview;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -9,32 +11,55 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.normalrecyclerview.R;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 public class CardAdapter extends RecyclerView.Adapter<CardAdapter.PlanetHolder2> {
+    private Context context2;
+    private ArrayList<PlanetCards> planetCardsArrayList;
+
+    public CardAdapter(Context context2, ArrayList<PlanetCards> planetCardsArrayList) {
+        this.context2 = context2;
+        this.planetCardsArrayList = planetCardsArrayList;
+    }
 
     @NonNull
     @Override
     public PlanetHolder2 onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context2).inflate(R.layout.item_card , parent , false);
+        return new PlanetHolder2(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull PlanetHolder2 holder, int position) {
+        PlanetCards planetCards = planetCardsArrayList.get(position);
+        holder.SetDetails(planetCards);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return planetCardsArrayList.size();
     }
 
     public class PlanetHolder2 extends RecyclerView.ViewHolder{
         private TextView txtNameCard, txtDistanceCard, txtGravityCard, txtDiameterCard;
 
-        public PlanetHolder2(@NonNull View itemView, TextView txtNameCard, TextView txtDistanceCard, TextView txtGravityCard, TextView txtDiameterCard) {
+        public PlanetHolder2(@NonNull View itemView) {
             super(itemView);
              txtNameCard = itemView.findViewById(R.id.txtNameCard);
              txtDistanceCard = itemView.findViewById(R.id.txtDistanceCard);
+             txtGravityCard = itemView.findViewById(R.id.txtGravityCard);
              txtDiameterCard = itemView.findViewById(R.id.txtDiameterCard);
+        }
+        void SetDetails(PlanetCards planet){
+            txtNameCard.setText(planet.getPlanetName2());
+            txtDistanceCard.setText(String.format(Locale.US,
+                    "Distance from sun : %d Million KM", planet.getDistanceFromSun2()));
+            txtGravityCard.setText(String.format(Locale.US,
+                    "Surface Gravity : %d N/Kg", planet.getGravity2()));
+            txtDiameterCard.setText(String.format(Locale.US,
+                    "Diameter : %d KM", planet.getDiameter2()));
         }
     }
 }
--- app/src/main/java/com/example/normalrecyclerview/cardview/CardViewActivity.java
+++ app/src/main/java/com/example/normalrecyclerview/cardview/CardViewActivity.java
@@ -1,16 +1,66 @@
 package com.example.normalrecyclerview.cardview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.normalrecyclerview.R;
 
+import java.util.ArrayList;
+
 public class CardViewActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private CardAdapter adapter;
+    private ArrayList<PlanetCards> planetCardsArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_view);
+        
+        initializeCardView();
+
+    }
+
+    private void initializeCardView() {
+        recyclerView = findViewById(R.id.recyclerViewCardy);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        planetCardsArrayList = new ArrayList<>();
+        adapter = new CardAdapter(this , planetCardsArrayList);
+        recyclerView.setAdapter(adapter);
+        createDataForCards();
+    }
+
+    private void createDataForCards() {
+        // This Method is for adding the data to recyclerView
+        // let's make adapter and planet model class
+
+        PlanetCards planet = new PlanetCards("Earth", 150,10,12750);
+        planetCardsArrayList.add(planet);
+
+        // Adding some other info
+        planet = new PlanetCards("Jupiter", 778, 26, 143000);
+        planetCardsArrayList.add(planet);
+        planet = new PlanetCards("Mars", 228, 4, 6800);
+        planetCardsArrayList.add(planet);
+        planet = new PlanetCards("Pluto", 5900, 1, 2320);
+        planetCardsArrayList.add(planet);
+        planet = new PlanetCards("Venus", 108, 9, 12750);
+        planetCardsArrayList.add(planet);
+        planet = new PlanetCards("Saturn", 1429, 11, 120000);
+        planetCardsArrayList.add(planet);
+        planet = new PlanetCards("Mercury", 58, 4, 4900);
+        planetCardsArrayList.add(planet);
+        planet = new PlanetCards("Neptune", 4500, 12, 50500);
+        planetCardsArrayList.add(planet);
+        planet = new PlanetCards("Uranus", 2870, 9, 52400);
+        planetCardsArrayList.add(planet);
+
+        adapter.notifyDataSetChanged();
+
+
+
     }
 }
